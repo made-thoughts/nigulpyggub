27ffd471a6a9567632ecaa60af7ce534e69241b1
Nick Hensel <nickhensel25@icloud.com>
2024-09-13 18:54:35 +0200

    prog

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = {
+    self,
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+
+      perSystem = {
+        config,
+        lib,
+        pkgs,
+        system,
+        ...
+      }: let
+        javaVersion = 22;
+
+        jdk = pkgs."temurin-bin-${toString javaVersion}";
+        gradle = pkgs.gradle.override {java = jdk;};
+       in {
+         devShells.default = pkgs.mkShell {
+           name = "Jack";
+           packages = with pkgs; [git jdk gradle];
+         };
+       };
+    };
+}
---
+++ src/main/java/club/devcord/gamejam/Nigulpyggub.java
@@ -0,0 +1,16 @@
+package club.devcord.gamejam;
+
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Nigulpyggub extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public void onEnable() {
+    }
+}
